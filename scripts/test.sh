fcf5e87045a5c5eee1ea4058e02002752c57e62f
zhangjunzhuang <zhangjunzhuang@veredholdings.com>
2020-11-03 17:04:54 +0800

    chore: 跳过tag 生成步骤

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,41 +2,45 @@
 
 cd $(dirname $0)/..
 
-current_branch=`git branch --show-current`
-echo "确定要发布当前分支 $current_branch 吗？（yes, no）"
-read is_publish
-
-if [[ $is_publish = "yes" ]]
+status_info=`git status 2>&1`
+if [[ $status_info =~ "working tree clean" ]]
 then
-  status_info=`git status 2>&1`
-  if [[ $status_info =~ "working tree clean" ]]
-  then
-    echo "正在生成更新日志，升级版本号..."
-    npm run release --skip.tag
+    current_branch=`git branch --show-current`
+    echo "确定要发布当前分支 $current_branch 吗？（yes, no）"
+    read is_publish
 
-    echo "将代码推至远程代码仓库"
-    git push
-
-    if [[ $current_branch != "master" ]]
+    if [[ $is_publish = "yes" ]]
     then
-        echo "切至 master 分支"
-        git checkout master
+          status_info=`git status 2>&1`
+          
+          echo "正在升级版本号，生成更新日志 CHANGELOG.md ..."
+          npm run release --skip.tag
 
-        echo "将刚才的分支合并至 master 分支"
-        git merge $current_branch
+          echo "将代码推至远程代码仓库"
+          git push
+
+          if [[ $current_branch != "master" ]]
+          then
+              echo "切至 master 分支"
+              git checkout master
+
+              echo "将刚才的分支合并至 master 分支"
+              git merge $current_branch
+          fi
+
+          echo "请输入 tag 号"
+          read new_tag
+
+          echo "正在创建本地 tag $new_tag"
+          git tag $new_tag
+
+          echo "上传本地 tag 至 远程仓库"
+          git push origin $new_tag
+
+          echo "发布完成。"
+    else
+        echo "您取消了发布当前分支。"
     fi
-
-    echo "请输入 tag 号"
-    read new_tag
-
-    echo "正在创建本地 tag $new_tag"
-    git tag $new_tag
-
-    echo "上传本地 tag 至 远程仓库"
-    git push origin $new_tag
-  else
-    echo "请先提交完代码，才可发布当前分支。"
-  fi
 else
-  echo "您取消了发布当前分支。"
+    echo "请先提交完代码，才可发布当前分支。"
 fi
